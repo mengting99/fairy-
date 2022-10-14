22a22dd9b35285249d3b5900a0528a77004277fc
mengting99 <115445195+mengting99@users.noreply.github.com>
2022-10-14 20:40:18 +0800

    Add files via upload

---
+++ 计算n!.cpp
@@ -0,0 +1,14 @@
+﻿#include<stdio.h>
+int main()
+{
+	int i = 0;
+	int n = 0;
+	scanf_s("%d", &n);
+	int ret = 1;
+	for(i = 1; i <= n; i++)
+	{
+		ret = ret * i;
+		printf("ret=%d", ret);
+	}
+return 0;
+}
