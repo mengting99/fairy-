5cfd2dfcda2d415a5318f8ccb330a615cdbcc7d4
mengting99 <115445195+mengting99@users.noreply.github.com>
2022-10-16 22:06:12 +0800

    Add files via upload

---
+++ 二分法.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int m = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = left + right / 2;
+		if (arr[mid] > m)
+		{
+			right = sz - 1;
+		}
+		else if (arr[mid] < m)
+		{
+			left = sz + 1;
+		}
+		else
+		{
+			printf("找到了,下标是：%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到\n");
+	}
+	return 0;
+}
