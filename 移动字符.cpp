b405ab716284a522265b5537d22830d0ca961b9d
mengting99 <115445195+mengting99@users.noreply.github.com>
2022-10-17 21:45:07 +0800

    Add files via upload

---
+++ 移动字符.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+#include<windows.h>
+int main()
+{
+	char arr1[] = "Bonnie is a beacutiful girl";
+	char arr2[] = "$$$$$$$$$$$$$$$$$$$$$$$$$$$";
+	int left = 0;
+	int right = sizeof(arr1) / sizeof(arr1[0]) - 2;
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		Sleep(1000);
+		left++;
+		right--;
+		if (left > right)
+			break;
+	}
+
+
+}
