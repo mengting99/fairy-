b2961b8e3a3271092c483aebf86ed12c9c5b146c
Bonnie <115445195+mengting99@users.noreply.github.com>
2022-10-24 21:51:29 +0800

    Add files via upload

---
+++ 找公约数.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf_s("%d%d", &a, &b);
+	while (c = a % b)
+	{
+		b = a;
+		c = b;
+		printf("%d", c);
+	}
+	printf("%d", b);
+	return 0;
+}
