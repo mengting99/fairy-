33e83008191ecccedcaeabe701ea15eb2aac4e90
Bonnie <115445195+mengting99@users.noreply.github.com>
2022-11-12 14:50:35 +0800

    Add files via upload

---
+++ 传址调用.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+void Swap(int* x, int* y)
+{
+	int tmp = 0;
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+int main()
+{
+	int a = 12;
+	int b = 28;
+	printf("交换前：\na=%d  b=%d\n", a, b);
+	Swap(&a, &b);
+	printf("交换后：\na=%d  b=%d", a, b);
+
+}
