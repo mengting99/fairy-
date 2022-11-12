628222c83f05b78b367e531c3f64e641ddb6fbbe
Bonnie <115445195+mengting99@users.noreply.github.com>
2022-11-12 11:29:20 +0800

    Add files via upload

---
+++ 二维排字母2.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[20] = {};
+	char str[3][20] = {};
+	int i = 0;
+	for (i = 0; i <3; i++)
+		gets_s(str[i]);
+	if (strcmp(str[0], str[1]) > 0)
+		strcpy_s(string, str[0]);
+	else
+		strcpy_s(string, str[1]);
+	if (strcmp(str[1], str[2]) < 0)
+		strcpy_s(string, str[2]);
+	printf("The largest word is:\n%s", string);
+	return 0;
+
+
+}
