28ec64624c2d2c00fa584791977895c624ebe955
Bonnie <115445195+mengting99@users.noreply.github.com>
2022-11-12 14:51:03 +0800

    Add files via upload

---
+++ 函数闰年.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+int year(int y)
+{
+	
+	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+		return 1;
+	else
+		return 0;
+	
+}
+int main()
+{
+	int y;
+	for (y = 1000; y < 2022; y++)
+	{
+		if(1==year(y))
+		printf("%d  ", y);
+}
+	return 0;
+}
