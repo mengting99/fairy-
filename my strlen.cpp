ea34d513de4bfade072f62d0ef97e71e7cd48fa5
Bonnie <115445195+mengting99@users.noreply.github.com>
2022-12-15 09:39:28 +0800

    Add files via upload

---
+++ my strlen.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include<assert.h>
+int count = 0;
+
+char my_strlen(char* dest)
+{
+    assert(dest != NULL);
+
+	while(*dest++ !='\0')
+   count++;
+    return count;
+}
+
+
+int main()
+{
+    char arr[20] = { "abcd" };
+	
+    int ret=my_strlen(arr);
+	printf("strlen=%d", ret);
+    return 0;
+}
