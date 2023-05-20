a0c0c13433a14d97dc0657206585f5a93a2d29ef
Bonnie <115445195+mengting99@users.noreply.github.com>
2023-05-20 19:00:22 +0800

    Add files via upload

---
+++ 排序.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef int datatype;
+#define maxsize 10
+
+typedef struct {
+    datatype array[maxsize];
+    int length;
+} sequenlist;
+
+// 初始化顺序表
+void initList(sequenlist* list) {
+    list->length = 0;
+}
+
+// 判断顺序表是否为空
+int isEmpty(sequenlist list) {
+    return list.length == 0;
+}
+
+// 判断顺序表是否已满
+int isFull(sequenlist list) {
+    return list.length == maxsize;
+}
+
+// 入栈操作
+int push(sequenlist* list, datatype data) {
+    if (isFull(*list)) {
+        printf("Stack is full.\n");
+        return 0;
+    }
+}
